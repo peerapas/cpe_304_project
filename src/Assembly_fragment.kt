3dc5f068bc1e7fb505911ae58cc5def774146509
peerapas <peerapas1998@gmail.com>
2019-11-05 21:03:30 +0700

    check instruction type

--- src/Assembly_fragment.kt
+++ src/Assembly_fragment.kt
@@ -1,23 +1,28 @@
 import java.io.File
 
+fun convertCode(){
 
+}
 fun main() {
     File("src/assembly.txt").forEachLine {
         var instructionLine = it.split(" ")
         if(instructionLine.size == 4)
         {
+//            check for r$i type
             var instruction = instructionLine[0]
             var field0 = instructionLine[1]
             var field1 = instructionLine[2]
             var field2 = instructionLine[3]
             println(instruction + field0 + field1 + field2)
         }
+//        check for j type
         else if(instructionLine.size == 3)
         {
             var instruction = instructionLine[0]
             var field0 = instructionLine[1]
             var field2 = instructionLine[2]
         }
+//        check for o type
         else if(instructionLine.size == 1)
         {
             var instruction = instructionLine[0]
