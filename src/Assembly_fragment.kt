1dfaa22a52c3f27ace5f48e9b5cea5f1e4b18317
peerapas <peerapas1998@gmail.com>
2019-11-05 23:12:45 +0700

    make it more OOP

--- src/Assembly_fragment.kt
+++ src/Assembly_fragment.kt
@@ -3,29 +3,41 @@ import java.io.File
 fun convertCode(){
 
 }
+
+
+fun loadInstructionModel(instructionLine:List<String>):instructionModel{
+    var instruction :String? = null
+    var field0 :String? = null
+    var field1 :String? = null
+    var field2 :String? = null
+    if(instructionLine.size == 4)
+    {
+//            check for r$i type
+        instruction = instructionLine[0]
+        field0 = instructionLine[1]
+        field1 = instructionLine[2]
+        field2 = instructionLine[3]
+    }
+//        check for j type
+    else if(instructionLine.size == 3)
+    {
+        instruction = instructionLine[0]
+        field0 = instructionLine[1]
+        field1 = instructionLine[2]
+    }
+//        check for o type
+    else if(instructionLine.size == 1)
+    {
+        instruction = instructionLine[0]
+    }
+    return instructionModel(instruction, field0, field1, field2)
+}
+
+
+
 fun main() {
     File("src/assembly.txt").forEachLine {
         var instructionLine = it.split(" ")
-        if(instructionLine.size == 4)
-        {
-//            check for r$i type
-            var instruction = instructionLine[0]
-            var field0 = instructionLine[1]
-            var field1 = instructionLine[2]
-            var field2 = instructionLine[3]
-            println(instruction + field0 + field1 + field2)
-        }
-//        check for j type
-        else if(instructionLine.size == 3)
-        {
-            var instruction = instructionLine[0]
-            var field0 = instructionLine[1]
-            var field2 = instructionLine[2]
-        }
-//        check for o type
-        else if(instructionLine.size == 1)
-        {
-            var instruction = instructionLine[0]
-        }
+        var instructionModel = loadInstructionModel(instructionLine)
     }
 }
---
+++ src/instructionModel.kt
@@ -0,0 +1,6 @@
+class instructionModel(instruction:String?, field0:String?, field1:String?, field2:String?) {
+    var instruction :String?= instruction
+    var field0 :String? = field0
+    var field1 :String? = field1
+    var field2 :String? = field2
+}
