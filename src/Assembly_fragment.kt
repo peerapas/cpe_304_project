22f466a2fed0fc19f13b660d881308fe29531652
peerapas <peerapas1998@gmail.com>
2019-11-06 13:39:06 +0700

    move loadInstructionModel to class constructor

--- src/Assembly_fragment.kt
+++ src/Assembly_fragment.kt
@@ -1,43 +1,12 @@
 import java.io.File
 
-fun convertCode(){
-
-}
-
-
-fun loadInstructionModel(instructionLine:List<String>):instructionModel{
-    var instruction :String? = null
-    var field0 :String? = null
-    var field1 :String? = null
-    var field2 :String? = null
-    if(instructionLine.size == 4)
-    {
-//            check for r$i type
-        instruction = instructionLine[0]
-        field0 = instructionLine[1]
-        field1 = instructionLine[2]
-        field2 = instructionLine[3]
-    }
-//        check for j type
-    else if(instructionLine.size == 3)
-    {
-        instruction = instructionLine[0]
-        field0 = instructionLine[1]
-        field1 = instructionLine[2]
-    }
-//        check for o type
-    else if(instructionLine.size == 1)
-    {
-        instruction = instructionLine[0]
-    }
-    return instructionModel(instruction, field0, field1, field2)
-}
-
-
 
 fun main() {
     File("src/assembly.txt").forEachLine {
         var instructionLine = it.split(" ")
-        var instructionModel = loadInstructionModel(instructionLine)
+        var instructionModel = instructionModel(instructionLine)
+        println(instructionModel.instruction)
+        val opcodeMap = mapOf<String, String>("add" to "000", "nand" to "001", "lw" to "010", "sw" to "011","beq" to "100", "jalr" to "101", "halt" to "110", "noop" to "111")
+
     }
 }
--- src/instructionModel.kt
+++ src/instructionModel.kt
@@ -1,6 +1,33 @@
-class instructionModel(instruction:String?, field0:String?, field1:String?, field2:String?) {
-    var instruction :String?= instruction
-    var field0 :String? = field0
-    var field1 :String? = field1
-    var field2 :String? = field2
+//class instructionModel(instruction:String?, field0:String?, field1:String?, field2:String?) {
+class instructionModel {
+
+    var instruction :String? = null
+    var field0 :String? = null
+    var field1 :String? = null
+    var field2 :String? = null
+
+    constructor(instructionLine: List<String>){
+        if(instructionLine.size == 4)
+        {
+//            check for r$i type
+            instruction = instructionLine[0]
+            field0 = instructionLine[1]
+            field1 = instructionLine[2]
+            field2 = instructionLine[3]
+        }
+//        check for j type
+        else if(instructionLine.size == 3)
+        {
+            instruction = instructionLine[0]
+            field0 = instructionLine[1]
+            field1 = instructionLine[2]
+        }
+//        check for o type
+        else if(instructionLine.size == 1)
+        {
+            instruction = instructionLine[0]
+        }
+
+    }
+
 }
