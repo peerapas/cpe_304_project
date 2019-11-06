9119f02937aa70883ecebe707a2e354862d1fdbd
peerapas <peerapas1998@gmail.com>
2019-11-06 14:04:27 +0700

    -add map of opcode -add generate opcode from instruction

--- src/Assembly_fragment.kt
+++ src/Assembly_fragment.kt
@@ -5,8 +5,10 @@ fun main() {
     File("src/assembly.txt").forEachLine {
         var instructionLine = it.split(" ")
         var instructionModel = instructionModel(instructionLine)
-        println(instructionModel.instruction)
-        val opcodeMap = mapOf<String, String>("add" to "000", "nand" to "001", "lw" to "010", "sw" to "011","beq" to "100", "jalr" to "101", "halt" to "110", "noop" to "111")
-
+//        println(instructionModel.instruction)
+        val opcodeMap = mapOf<String?, String?>("add" to "000", "nand" to "001", "lw" to "010", "sw" to "011","beq" to "100", "jalr" to "101", "halt" to "110", "noop" to "111")
+        if(opcodeMap.containsKey(instructionModel.instruction)){
+            println(opcodeMap.getValue(instructionModel.instruction))
+        }
     }
 }
