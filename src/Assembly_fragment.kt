7a2e8ed362bd0c8a7f97dd74644b9dd94e441023
peerapas <peerapas1998@gmail.com>
2019-11-08 18:22:13 +0700

    -add convert R type instruction to machine code

--- src/Assembly_fragment.kt
+++ src/Assembly_fragment.kt
@@ -1,4 +1,65 @@
 import java.io.File
+import kotlin.test.fail
+
+val opcodeMap = mapOf<String?,List<Int>>(
+    "add" to listOf<Int>(0,0,0),
+    "nand" to listOf<Int>(0,0,1),
+    "lw" to listOf<Int>(0,1,0),
+    "sw" to listOf<Int>(0,1,1),
+    "beq" to listOf<Int>(1,0,0),
+    "jalr" to listOf<Int>(1,0,1),
+    "halt" to listOf<Int>(1,1,0),
+    "noop" to listOf<Int>(1,1,1)
+)
+
+fun convertRtype(inputInstruction:instructionModel):MutableList<Int>{
+    val outputBinary :MutableList<Int> = mutableListOf()
+    if(opcodeMap.containsKey(inputInstruction.instruction)){
+        outputBinary.addAll(opcodeMap.getValue(inputInstruction.instruction))
+        outputBinary.addAll(toBinary(inputInstruction.field0.toInt()))
+        outputBinary.addAll(toBinary(inputInstruction.field1.toInt()))
+        outputBinary.addAll(listOf(0,0,0,0,0,0,0,0,0,0,0,0,0))
+        outputBinary.addAll(toBinary(inputInstruction.field2.toInt()))
+    }
+    return outputBinary
+}
+
+fun convertItype(inputInstruction: instructionModel, instructionCollection: MutableList<instructionModel>):MutableList<Int>{
+    val outputBinary :MutableList<Int> = mutableListOf<Int>()
+    if(inputInstruction.field2.toIntOrNull() == null)
+    {
+        for(instruction in instructionCollection)
+        {
+            if(inputInstruction.field2 == instruction.label)
+            {
+
+            }
+        }
+    }
+    else
+    {
+        if(opcodeMap.containsKey(inputInstruction.instruction)) {
+            outputBinary.addAll(opcodeMap.getValue(inputInstruction.instruction))
+            outputBinary.addAll(toBinary(inputInstruction.field0.toInt()))
+            outputBinary.addAll(toBinary(inputInstruction.field1.toInt()))
+            outputBinary.addAll(listOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+            outputBinary.addAll(toBinary(inputInstruction.field2.toInt()))
+        }
+    }
+    return outputBinary
+}
+
+fun convertJtype(){
+
+}
+
+fun convertOtype(){
+
+}
+
+fun convertFillType(){
+
+}
 
 fun toBinary(input: Int):MutableList<Int> {
     var dec :Int = input
@@ -17,7 +78,6 @@ fun toBinary(input: Int):MutableList<Int> {
             dec = dec/2
         }
 
-//        binary.add(1)
         if(binary.size == 2){
             binary.add(0)
             binary.reverse()
@@ -31,29 +91,33 @@ fun toBinary(input: Int):MutableList<Int> {
 }
 
 
-fun main() {
+fun main(){
+    val instructionCollection:MutableList<instructionModel> = mutableListOf()
     File("src/assembly.txt").forEachLine {
         var binary = mutableListOf<Int>(0,0,0,0,0,0,0)
         var hex = mutableListOf<Int>(0)
         var instructionLine = it.split(" " )
         var instructionModel = instructionModel(instructionLine)
-        val opcodeMap = mapOf<String?,List<Int>>(
-            "add" to listOf<Int>(0,0,0),
-            "nand" to listOf<Int>(0,0,1),
-            "lw" to listOf<Int>(0,1,0),
-            "sw" to listOf<Int>(0,1,1),
-            "beq" to listOf<Int>(1,0,0),
-            "jalr" to listOf<Int>(1,0,1),
-            "halt" to listOf<Int>(1,1,0),
-            "noop" to listOf<Int>(1,1,1)
-        )
+        instructionCollection.add(instructionModel)
+//        println(instructionModel.instruction)
 //      push opcode to line of binary
-        if(opcodeMap.containsKey(instructionModel.instruction)){
-            binary.addAll(opcodeMap.getValue(instructionModel.instruction))
-            binary.addAll(toBinary(instructionModel.field0.toInt()))
-            println(binary)
-
-        }
-
     }
+    for(instruction in instructionCollection){
+        if(instruction.type == "R")
+        {
+            println(convertRtype(instruction))
+        }
+    }
+
+//    for(i in instructionCollection){
+//        if(i.field2.toIntOrNull() == null && i.field2 != "") {
+////            println(i.field2)
+//            for(j in instructionCollection){
+//                if(i.field2 == j.label)
+//                {
+//                    println(instructionCollection.indexOf(j))
+//                }
+//            }
+//        }
+//    }
 }
--- src/instructionModel.kt
+++ src/instructionModel.kt
@@ -6,6 +6,7 @@ class instructionModel {
     var field0 :String = ""
     var field1 :String = ""
     var field2 :String = ""
+    var type :String = ""
 
     constructor(instructionLine: List<String>){
         if(instructionLine.size == 5)
@@ -16,6 +17,14 @@ class instructionModel {
             field0 = instructionLine[2]
             field1 = instructionLine[3]
             field2 = instructionLine[4]
+            if(instructionLine.contains("add") || instructionLine.contains("nand"))
+            {
+                type = "R"
+            }
+            else
+            {
+                type = "I"
+            }
         }
 //        check for j type
         else if(instructionLine.size == 4)
@@ -24,14 +33,22 @@ class instructionModel {
             instruction = instructionLine[1]
             field0 = instructionLine[2]
             field1 = instructionLine[3]
+            type = "J"
         }
 //        check for o type
         else if(instructionLine.size == 2)
         {
             label = instructionLine[0]
             instruction = instructionLine[1]
+            type = "O"
         }
-
+        else if(instructionLine.contains(".fill"))
+        {
+            label = instructionLine[0]
+            instruction = instructionLine[1]
+            field0 = instructionLine[2]
+            type = ".fill"
+        }
     }
 
 }
