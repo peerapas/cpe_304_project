07541c8fb510d2f9ee6b6964e347d750e901e958
peerapas <peerapas1998@gmail.com>
2019-11-08 19:41:43 +0700

    -convert instruction to binary

--- src/Assembly_fragment.kt
+++ src/Assembly_fragment.kt
@@ -1,6 +1,7 @@
 import java.io.File
 import kotlin.test.fail
 
+
 val opcodeMap = mapOf<String?,List<Int>>(
     "add" to listOf<Int>(0,0,0),
     "nand" to listOf<Int>(0,0,1),
@@ -13,7 +14,7 @@ val opcodeMap = mapOf<String?,List<Int>>(
 )
 
 fun convertRtype(inputInstruction:instructionModel):MutableList<Int>{
-    val outputBinary :MutableList<Int> = mutableListOf()
+    val outputBinary :MutableList<Int> = mutableListOf<Int>(0,0,0,0,0,0,0)
     if(opcodeMap.containsKey(inputInstruction.instruction)){
         outputBinary.addAll(opcodeMap.getValue(inputInstruction.instruction))
         outputBinary.addAll(toBinary(inputInstruction.field0.toInt()))
@@ -25,14 +26,18 @@ fun convertRtype(inputInstruction:instructionModel):MutableList<Int>{
 }
 
 fun convertItype(inputInstruction: instructionModel, instructionCollection: MutableList<instructionModel>):MutableList<Int>{
-    val outputBinary :MutableList<Int> = mutableListOf<Int>()
+    val outputBinary :MutableList<Int> = mutableListOf<Int>(0,0,0,0,0,0,0)
     if(inputInstruction.field2.toIntOrNull() == null)
     {
         for(instruction in instructionCollection)
         {
             if(inputInstruction.field2 == instruction.label)
             {
-
+                outputBinary.addAll(opcodeMap.getValue(inputInstruction.instruction))
+                outputBinary.addAll((toBinary(inputInstruction.field0.toInt())))
+                outputBinary.addAll(toBinary(inputInstruction.field1.toInt()))
+                outputBinary.addAll(listOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+                outputBinary.addAll(toBinary(instructionCollection.indexOf(instruction)))
             }
         }
     }
@@ -49,16 +54,38 @@ fun convertItype(inputInstruction: instructionModel, instructionCollection: Muta
     return outputBinary
 }
 
-fun convertJtype(){
-
+fun convertJtype(inputInstruction: instructionModel):MutableList<Int>{
+    val outputBinary :MutableList<Int> = mutableListOf<Int>(0,0,0,0,0,0,0)
+    outputBinary.addAll(opcodeMap.getValue(inputInstruction.instruction))
+    outputBinary.addAll((toBinary(inputInstruction.field0.toInt())))
+    outputBinary.addAll(toBinary(inputInstruction.field1.toInt()))
+    outputBinary.addAll(listOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+    return outputBinary
 }
 
-fun convertOtype(){
-
+fun convertOtype(inputInstruction: instructionModel):MutableList<Int>{
+    val outputBinary :MutableList<Int> = mutableListOf<Int>(0,0,0,0,0,0,0)
+    outputBinary.addAll(opcodeMap.getValue(inputInstruction.instruction))
+    outputBinary.addAll(listOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+    return outputBinary
 }
 
-fun convertFillType(){
-
+fun convertFillType(inputInstruction: instructionModel, instructionCollection: MutableList<instructionModel>):Int?{
+    var output:Int? = null
+    if(inputInstruction.field0.toIntOrNull() == null)
+    {
+        for(instruction in instructionCollection){
+            if(inputInstruction.field0 == instruction.label)
+            {
+                output = instructionCollection.indexOf(instruction)
+            }
+        }
+    }
+    else
+    {
+        output = inputInstruction.field0.toInt()
+    }
+    return output
 }
 
 fun toBinary(input: Int):MutableList<Int> {
@@ -94,7 +121,7 @@ fun toBinary(input: Int):MutableList<Int> {
 fun main(){
     val instructionCollection:MutableList<instructionModel> = mutableListOf()
     File("src/assembly.txt").forEachLine {
-        var binary = mutableListOf<Int>(0,0,0,0,0,0,0)
+
         var hex = mutableListOf<Int>(0)
         var instructionLine = it.split(" " )
         var instructionModel = instructionModel(instructionLine)
@@ -106,6 +133,22 @@ fun main(){
         if(instruction.type == "R")
         {
             println(convertRtype(instruction))
+        }
+        else if(instruction.type == "I")
+        {
+            println(convertItype(instruction, instructionCollection))
+        }
+        else if(instruction.type == "J")
+        {
+            println(convertJtype(instruction))
+        }
+        else if(instruction.type == "O")
+        {
+            println(convertOtype(instruction))
+        }
+        else if(instruction.type == ".fill")
+        {
+            println(convertFillType(instruction, instructionCollection))
         }
     }
 
